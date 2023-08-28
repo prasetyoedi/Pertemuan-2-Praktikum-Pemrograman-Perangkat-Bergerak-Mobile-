2d7dff059724844917f5efc7eeb7a0ba1e6ae61c
Prasetyo Edi Pamungkas <prasetyoedi451@gmail.com>
2023-08-29 00:18:07 +0700

    Up Tugas Pertemuan 2

--- app/src/main/java/com/example/pertemuan2/MainActivity.kt
+++ app/src/main/java/com/example/pertemuan2/MainActivity.kt
@@ -1,57 +1,36 @@
 package com.example.pertemuan2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.Toast
+import androidx.activity.ComponentActivity
 import com.example.pertemuan2.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     private lateinit var binding: ActivityMainBinding
-    private var number = 0
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        buat tanpa binding
-//        val btnToast = findViewById<Button>(R.id.btn_toast)
+        with(binding) {
+            val adminUsername = "admin"
+            val adminPasswd = "123"
 
+            loginBtn.setOnClickListener{
+                val valueUsername = usn.text.toString()
+                val valuePasswd = pass.text.toString()
 
-        with(binding){
-            txtNumber.text = number.toString()
-           // ubah bg color
-
-            txtNumber.setBackgroundResource(R.color.purple_200)
-
-            val email = "admin"
-            val pass = "pw123"
-
-            btnGetText.setOnClickListener{
-                val valueText = editEmail.text.toString()
-
-                // show toast
-                Toast.makeText(
-                    this@MainActivity,
-                    valueText,
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-
-
-            btnCount.setOnClickListener(){
-                number++
-                txtNumber.text = number.toString()
-                if(number % 2 == 0){
-                    txtNumber.setBackgroundResource(R.color.purple_200)
-                }else{
-                    txtNumber.setBackgroundResource(R.color.purple_500)
+                if (valueUsername == adminUsername && valuePasswd == adminPasswd) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Login Sukses",
+                        Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Login Gagal",
+                        Toast.LENGTH_SHORT).show()
                 }
-            }
-
-            btnToast.setOnClickListener{
-                Toast.makeText(this@MainActivity, number.toString(), Toast.LENGTH_SHORT).show()
             }
         }
     }
